2392a3ad7bf13ba612046667455173cb5511817f
tobyyho <81172666+tobyyho@users.noreply.github.com>
2021-05-09 21:20:09 -0400

    Create BenfordsAssignment.java

---
+++ BenfordsAssignment.java
@@ -0,0 +1,10 @@
+/*
+* Name : Toby and Dhruvika
+* Teacher: Mr.Ho
+* Date: May 12th
+* Description: Create a graph, showing the percentages of each number being shown up in a file given to us
+*/
+
+
+class BenfordsAssignement{
+}
