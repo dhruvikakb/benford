8a006d06c67adf6fff408c7f58038f740ee49730
Toby Ho <tobyho@Tobys-MacBook-Pro.local>
2021-05-10 12:39:00 -0400

    Printed percentages of each number's occurence in the first digit, to the first decimal place

--- BenfordsLawAssignment.java
+++ BenfordsLawAssignment.java
@@ -1,10 +1,10 @@
 /*
- * Date: 
- * Name: 
- * Teacher: 
- * Description: 
- *   
- */ 
+* Name : Toby and Dhruvika
+* Teacher: Mr.Ho
+* Date: May 12th
+* Description: Create a graph, showing the percentages of each number being shown up in a file given to us
+*/
+
 import java.io.File;
 import java.util.Scanner;
 
@@ -61,3 +61,23 @@ class BenfordJava {
                 nine++;
             }
         }
+        double percentOne = Math.round((one / total * 100) * 10) / 10.0;
+            System.out.println(percentOne);
+            double percentTwo = Math.round((two / total * 100) * 10) / 10.0;
+            System.out.println(percentTwo);
+            double percentThree = Math.round((three / total * 100) * 10) / 10.0;
+            System.out.println(percentThree);
+            double percentFour = Math.round((four / total * 100) * 10) / 10.0;
+            System.out.println(percentFour);
+            double percentFive = Math.round((five / total * 100) * 10) / 10.0;
+            System.out.println(percentFive);
+            double percentSix = Math.round((six / total * 100) * 10) / 10.0;
+            System.out.println(percentSix);
+            double percentSeven = Math.round((seven / total * 100) * 10) / 10.0;
+            System.out.println(percentSeven);
+            double percentEight = Math.round((eight / total * 100) * 10) / 10.0;
+            System.out.println(percentEight);
+            double percentNine = Math.round((nine / total * 100) * 10) / 10.0;
+            System.out.println(percentNine);
+    }
+}
