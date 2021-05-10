5bce1b1b9dab7ae48da0e4629c00279861f1a7ba
dhruvikakb <83921408+dhruvikakb@users.noreply.github.com>
2021-05-10 12:05:18 -0400

    Add files via upload

--- BenfordsLawAssignment.java
+++ BenfordsLawAssignment.java
@@ -14,7 +14,6 @@ class BenfordJava {
     public static void man(String[] args)throws Exception {
 
         //Set vairables to 0 once created 
-
         double one =0;
         double two=0;
         double three = 0;
@@ -62,38 +61,3 @@ class BenfordJava {
                 nine++;
             }
         }
-        
-
-        double percentOne = Math.round((one / total * 100) * 10) / 10.0;
-        System.out.println(percentOne);
-
-        double percentTwo = Math.round((two / total * 100) * 10) / 10.0;
-        System.out.println(percentTwo);
-
-        double percentThree = Math.round((three / total * 100) * 10) / 10.0;
-        System.out.println(percentThree);
-
-        double percentFour = Math.round((four / total * 100) * 10) / 10.0;
-        System.out.println(percentFour);
-
-        double percentFive = Math.round((five / total * 100) * 10) / 10.0;
-        System.out.println(percentFive);
-
-        double percentSix = Math.round((six / total * 100) * 10) / 10.0;
-        System.out.println(percentSix);
-
-        double percentSeven = Math.round((seven / total * 100) * 10) / 10.0;
-        System.out.println(percentSeven);
-
-        double percentEight = Math.round((eight / total * 100) * 10) / 10.0;
-        System.out.println(percentEight);
-        
-        double percentNine = Math.round((nine / total * 100) * 10) / 10.0;
-        System.out.println(percentNine);
-    }
-}
-
-
-    }
-    
-}
