80948db3833793a1d09592c9eb164076e0afa3d1
tobyyho <81172666+tobyyho@users.noreply.github.com>
2021-05-10 12:22:32 -0400

    Update BenfordsLawAssignment.java

--- BenfordsLawAssignment.java
+++ BenfordsLawAssignment.java
@@ -11,11 +11,11 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 class BenfordJava {
-    public static void man(String[] args)throws Exception {
+    public static void main(String[] args)throws Exception {
 
         //Set vairables to 0 once created 
         double one =0;
-        double two=0;
+        double two = 0;
         double three = 0;
         double four = 0; 
         double five = 0;
@@ -31,33 +31,33 @@ class BenfordJava {
 
         while(scan.hasNext()){
             total++;
-            String a = Character.toString(scan.next().charAt(4));
+            String num = Character.toString(scan.next().charAt(4));
          
-            if(a.equals("1")){
+            if(num.equals("1")){
                 one++;
             }
-            if(a.equals("2")){
+            if(num.equals("2")){
                 two++;
             }
-            if(a.equals("3")){
+            if(num.equals("3")){
                 three++;
             }
-            if(a.equals("4")){
+            if(num.equals("4")){
                 four++;
             }
-            if(a.equals("5")){
+            if(num.equals("5")){
                 five++;
             }
-            if(a.equals("6")){
+            if(num.equals("6")){
                 six++;
             }
-            if(a.equals("7")){
+            if(num.equals("7")){
                 seven++;
             }
-            if(a.equals("8")){
+            if(num.equals("8")){
                 eight++;
             }
-            if(a.equals("9")){
+            if(num.equals("9")){
                 nine++;
             }
         }
